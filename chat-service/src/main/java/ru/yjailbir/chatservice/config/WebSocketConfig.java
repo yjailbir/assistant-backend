8b28b3cc471a2acd27038b4a42e18e1ea2f75ae4
yjailbir <nkudashko@mail.ru>
2026-02-23 16:36:54 +0300

    Добавил очередь

--- chat-service/src/main/java/ru/yjailbir/chatservice/config/WebSocketConfig.java
+++ chat-service/src/main/java/ru/yjailbir/chatservice/config/WebSocketConfig.java
@@ -33,7 +33,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic", "/queue");
     }
 
     @Override
