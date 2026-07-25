3a2cbcb7693fc7a049fcbf98e5dab906e6e36c8a
yjailbir <nkudashko@mail.ru>
2026-07-25 17:28:57 +0300

    Нуллабельные контракты

--- chat-service/src/main/java/ru/yjailbir/chatservice/dto/ChatSummaryDto.java
+++ chat-service/src/main/java/ru/yjailbir/chatservice/dto/ChatSummaryDto.java
@@ -1,13 +1,19 @@
 package ru.yjailbir.chatservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.time.Instant;
 
 public record ChatSummaryDto(
         String sessionId,
         SessionStatus status,
+        @Schema(types = {"string", "null"})
         String participantName,
+        @Schema(types = {"string", "null"})
         String lastMessageContent,
+        @Schema(types = {"string", "null"})
         Instant lastMessageTimestamp,
+        @Schema(types = {"string", "null"})
         String lastMessageSender
 ) {
 }
